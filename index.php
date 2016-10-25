a3110f10fc2f518f7934408e6332e5aa104247c7
Aleksandr S Melnik <alexmelnik224@gmail.com>
2016-10-25 12:12:43 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Papa Melnik's Website</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!--  There are no external links to css or js-->
+  <link rel="stylesheet" href="dist/css/bootstrap.min.css">
+   <link rel="stylesheet" href="dist/css/custom.css">
+  <script src="dist/jquery/jquery.min.js"></script>
+  <script src="dist/js/bootstrap.min.js"></script>
+  <!-- *********************************** -->
+</head>
+<body>
+<!--  add a menu here -->
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+}
+
+li {
+    float: left;
+}
+
+li a {
+    display: inline-block;
+    color: lighblue;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+li a:hover {
+    background-color: #000;
+}
+</style>
+</head>
+<body>
+
+<ul>
+  <li><a href="#home">Home</a></li>
+  <li><a href="phpcode">PHP Code</a></li>
+  <li><a href="downloads">Downloads</a></li>
+  <li><a href="javascript">Java Script</a></li>
+  <li><a href="templates">Templates</a></li>
+  <li><a href="pma">PMA</a></li>
+  <li><a href="contact.html">Contact</a></li>
+  <li><a href="info.html">Info</a></li>
+</ul>
+
+</body>
+<body style="background-color:cyan;"></body>
+</html>
+
+
+
+<!-- end of menu if is the iframe-->
+<!-- do not change the following until you have met localhost checkoff #2 -->
+<div class="jumbotron text-center">
+  <h1>Papa Aleksandr Melnik's Website</h1>
+  <h2>Pro website deisgn</h2>
+<body>
+
+
+
+</body>
+  
+  <p></p>
+</div>
+  
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>Column 1</h3>
+      <div class="container">
+		<h2>Responsive Iframe</h2>
+			<div class="embed-responsive embed-responsive-16by9">
+				<iframe  class="embed-responsive-item" name="if" src="intro.html"  ></iframe>
+			</div>
+	</div>
+
+    </div>
+   
+</div>
+
+</body>
+</html>
+
+
